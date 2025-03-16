b73896f7eac5f834968e0e543570f9c8f9ba009f
Aliaksandr Shcherba <salsarodrigues@icloud.com>
2025-03-16 14:40:37 +0300

    feat: CliParser implementation

--- app/main.cpp
+++ app/main.cpp
@@ -1,4 +1,20 @@
-int main()
+#include <iostream>
+
+#include "cli/cli_parser.h"
+
+int main(int argc, char** argv)
 {
-    return 0;
+    using namespace itask::cli_parser;
+
+    try
+    {
+        auto args{CliParser{"itask", "ingenium coding task"}.parse(argc, argv)};
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
---
+++ itask_lib/cli/cli_parser.cpp
@@ -0,0 +1,29 @@
+#include "cli_parser.h"
+
+#include <cxxopts.hpp>
+
+using namespace itask::utils::types;
+
+namespace itask::cli_parser
+{
+    CliParser::CliParser(std::string appName, std::string appDescription) :
+        appName_(std::move(appName)), appDescription_(std::move(appDescription))
+    {
+    }
+
+    CliArgs itask::cli_parser::CliParser::parse(int argc, char** argv)
+    {
+        cxxopts::Options options(appName_, appDescription_);
+        options.add_options()
+            ("p,path", "JSON file path", cxxopts::value<std::string>());
+
+        auto result = options.parse(argc, argv);
+        if (!result.count("path"))
+        {
+            throw std::invalid_argument("JSON file path is required. Use --path or -p to specify it");
+        }
+
+        std::string jsonFilePath{result["path"].as<std::string>()};
+        return {std::move(jsonFilePath)};
+    }
+}
