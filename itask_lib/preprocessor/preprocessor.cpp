87a69328554555b682a890d2b7f5bb895a822ea5
Aliaksandr Shcherba <salsarodrigues@icloud.com>
2025-03-16 21:05:40 +0300

    feat: Preprocessor implementation

---
+++ itask_lib/preprocessor/preprocessor.cpp
@@ -0,0 +1,149 @@
+#include "preprocessor.h"
+#include "utils/misc/misc.h"
+
+#include <nlohmann/json.hpp>
+#include <fstream>
+
+using namespace itask::utils::types;
+using namespace itask::utils::misc;
+using namespace nlohmann;
+
+namespace itask::preprocessor
+{
+    Preprocessor::Preprocessor(std::string filePath, const uint16_t threadCount, const uint64_t intervalRangeNanoSec) :
+        filePath_(std::move(filePath)), threadCount_(threadCount), intervalRangeNanoSec_(intervalRangeNanoSec)
+    {
+        if (filePath_.empty())
+        {
+            throw std::invalid_argument("Empty path for preprocessing");
+        }
+
+        if (threadCount_ == 0)
+        {
+            throw std::invalid_argument("Thread count must be positive");
+        }
+
+        if (intervalRangeNanoSec_ == 0)
+        {
+            throw std::invalid_argument("Interval range must be positive");
+        }
+    }
+
+    PreprocessedData Preprocessor::getPreprocessedData()
+    {
+        std::ifstream preprocFile(filePath_);
+        Defer deferClose(std::move([&preprocFile](){preprocFile.close();}));
+
+        if (!preprocFile.is_open())
+        {
+            throw std::runtime_error("Could not open preprocessing file: " + filePath_);
+        }
+
+        auto timeIntervals{getTimeIntervals_(preprocFile)};
+        auto fileSegments{getFileSegments_(preprocFile)};
+        return PreprocessedData{std::move(fileSegments), std::move(timeIntervals)};
+    }
+
+    std::vector<TimeInterval> Preprocessor::getTimeIntervals_(std::ifstream& file) const
+    {
+        file.seekg(0, std::ios::beg);
+
+        // first and last lines to get later first and last timestamp.
+        std::string firstLine, lastLine;
+
+        // process first line
+        std::getline(file, firstLine);
+
+        // process second line, first of all set read pointer to the before EOF position.
+        file.seekg(-2, std::ios::end); // (-1 before EOF)
+        char c;
+
+        // move to file beginning char by char
+        while (file.tellg() > 0)
+        {
+            // set previous position
+            file.seekg(-2, std::ios::cur);
+            file.get(c);
+
+            // if '\n' was found, that means we are ready to read last line.
+            if (c == '\n')
+                break;
+        }
+
+        // store last line
+        std::getline(file, lastLine);
+
+        // precess timestamps
+        uint64_t firstTimestamp{0};
+        uint64_t lastTimestamp{0};
+        {
+            json j = json::parse(firstLine);
+            firstTimestamp = static_cast<uint64_t>(stoll(j["time"]["$numberLong"].get<std::string>()));
+            j = json::parse(lastLine);
+            lastTimestamp = static_cast<uint64_t>(stoll(j["time"]["$numberLong"].get<std::string>()));
+        }
+
+        // timestamps was stored, ready to parse intervals.
+        // calculate value of intervals
+        uint64_t totalDuration{lastTimestamp - firstTimestamp};
+        uint64_t intervalsValue{totalDuration / intervalRangeNanoSec_};
+
+        // check remaining fraction of interval.
+        if (totalDuration % intervalRangeNanoSec_)
+        {
+            // we add one more interval to ensure all data is covered.
+            intervalsValue++;
+        }
+
+        // almost done, let's collect intervals collection
+        std::vector<TimeInterval> intervals;
+        intervals.reserve(intervalsValue);
+        for (int i = 0; i < intervalsValue; ++i)
+        {
+            uint64_t startPoint{firstTimestamp + i * intervalRangeNanoSec_};
+            uint64_t endPoint{startPoint + intervalRangeNanoSec_};
+            TimeInterval tmp{startPoint, endPoint};
+            intervals.emplace_back(std::move(tmp));
+        }
+        return intervals;
+    }
+
+    std::vector<FileSegment> Preprocessor::getFileSegments_(std::ifstream& file) const
+    {
+        // set read pointer to end of file and store file size
+        file.seekg(0, std::ios::end);
+        size_t fileSize = file.tellg();
+        size_t chunkSize = fileSize / threadCount_;
+
+        std::vector<FileSegment> fileSegments;
+        fileSegments.reserve(threadCount_);
+
+        for (int i = 0; i < threadCount_; ++i)
+        {
+            size_t startOffset = i * chunkSize;
+            size_t endOffset = (i == threadCount_ - 1) ? fileSize : (startOffset + chunkSize);
+
+            // move startOffset forward until reach '\n'
+            if (startOffset > 0)
+            {
+                file.seekg(startOffset);
+                char c;
+                while (file.get(c) && c != '\n' && file.tellg() < fileSize);
+                startOffset = file.tellg();
+            }
+
+            // move endOffset forward until reach '\n'
+            if (endOffset < fileSize)
+            {
+                file.seekg(endOffset);
+                char c;
+                while (file.get(c) && c != '\n' && file.tellg() < fileSize);
+                endOffset = file.tellg();
+            }
+
+            FileSegment tmp {startOffset, endOffset};
+            fileSegments.emplace_back(std::move(tmp));
+        }
+        return fileSegments;
+    }
+}
---
+++ itask_lib/preprocessor/preprocessor.h
@@ -0,0 +1,93 @@
+#ifndef PREPROCESSOR_H
+#define PREPROCESSOR_H
+
+#include "utils/types/preprocessor.h"
+
+#include<string>
+
+using namespace itask::utils::types;
+
+namespace itask::preprocessor
+{
+    /**
+     * @class Preprocessor
+     * @brief Prepares file partitions and time intervals for processing.
+     *
+     * The Preprocessor class is responsible for reading an input file,
+     * partitioning it into segments for further multithreaded processing, and
+     * defining time intervals for data unique classification.
+     */
+    class Preprocessor
+    {
+    public:
+        Preprocessor() = delete;
+        Preprocessor(const Preprocessor&) = delete;
+        Preprocessor(Preprocessor&&) = delete;
+        Preprocessor& operator=(const Preprocessor&) = delete;
+        Preprocessor& operator=(Preprocessor&&) = delete;
+
+        ~Preprocessor() = default;
+
+        /**
+         * @brief Constructs a Preprocessor instance.
+         *
+         * This class is responsible for preparing partitions for further parsing.
+         * It splits the input file into multiple partitions for the specified number of threads.
+         * And defines time intervals range to map records by specific interval id.
+         *
+         * @param filePath Path to the input file.
+         * @param threadCount Number of threads for processing.
+         *        The file will be evenly partitioned, with each thread assigned its own parsing chunk.
+         * @param intervalRangeNanoSec Interval range in nanoseconds.
+         *        This defines the time range for which records will be grouped and processed together,
+         *        Each record will belong to a specific interval based on its timestamp.
+         *
+         * @throws If the provided file path is empty or thread count is zero.
+         */
+        Preprocessor(std::string filePath, const uint16_t threadCount, const uint64_t intervalRangeNanoSec);
+
+        /**
+         * @brief Retrieves preprocessed data from a file.
+         *
+         * Reads a file and parses its contents to generate preprocessed data,
+         * including file segments and time intervals.
+         *
+         * @return Preprocessed data containing file segments and time intervals.
+         *
+         * @throws If the file cannot be opened, read, failed to parse data.
+         */
+        PreprocessedData getPreprocessedData();
+
+    private:
+        std::string filePath_{};
+        uint16_t threadCount_{0};
+        uint64_t intervalRangeNanoSec_{0};
+
+        /**
+         * @brief Parses time intervals from the given file stream.
+         *
+         * This method reads and extracts first and last timestamp from the provided input file stream.
+         * It processes the file content to generate a collection of time intervals.
+         *
+         * @param file Input file stream to read time intervals from.
+         * @return A vector of parsed time intervals.
+         *
+         * @throws If the file cannot be read or the parsing fails.
+         */
+        std::vector<TimeInterval> getTimeIntervals_(std::ifstream& file) const;
+
+        /**
+         * @brief Parses file segments from the given file stream.
+         *
+         * This method reads the input file and determines segments for partitioning.
+         *
+         * @param file Input file stream used to read and extract file segments.
+         * @return A vector of parsed file segments.
+         *
+         * @throws  If the file cannot be read or the parsing fails.
+         */
+        std::vector<FileSegment> getFileSegments_(std::ifstream& file) const;
+    };
+}
+
+#endif //PREPROCESSOR_H
